65be1cc80d294bddd0f6ee9b0e05172f8a76809f
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-16 13:16:58 +0530

    Update ExpenseForm.js

--- NewExpenses/ExpenseForm.js
+++ NewExpenses/ExpenseForm.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
+
+
 const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
