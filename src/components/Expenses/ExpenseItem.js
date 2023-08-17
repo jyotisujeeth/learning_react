dd14889c16fa81ccfc8e7ac9bb15b9d2c4d8fd20
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-17 12:40:32 +0530

    Update ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,17 +5,24 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-// function click handler
-const [amount, setamount] = useState(props.amount);
-console.log('ExpensesItem evaluated by React');
-const title = props.title;
+  // function click handler for amount
+  const [amount, setamount] = useState(props.amount);
+  console.log("ExpensesItem evaluated by React");
+  // const title = props.title;
 
   const clickHandler = () => {
-   setamount(100);
+    setamount(100);
     console.log(amount);
   };
+  // function click handler for title
+  const [title, setTitle] = useState(props.title);
 
- // document.
+  const clickHandlerT = () => {
+    setTitle("changeIt!");
+    console.log(title);
+  };
+
+  // document.
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -25,35 +32,10 @@ const title = props.title;
         <h2>{amount}</h2>
       </div>
       <button onClick={clickHandler}>changeAmount</button>
+      <button onClick={clickHandlerT}>changeTitle</button>;
       {/* <button onClick={() => {console.log("clicked ")}}> change title</button> */}
     </Card>
   );
 };
 
 export default ExpenseItem;
-
-
-
-// const ExpenseItem = (props) => {
-//   // function click handler
-//   const [title, setTitle] = useState(props.title);
-//   console.log("ExpensesItem evaluated by React");
-
-//   const clickHandler = () => {
-//     setTitle("changeIt!");
-//     console.log(title);
-//   };
-
-//   // document.
-//   return (
-//     <Card className="expense-item">
-//       <ExpenseDate date={props.date} />
-//       <div className="expense-item__description">
-//         <h2>{title}</h2>
-//         <div className="expense-item__price">${props.amount}</div>
-//       </div>
-//       <button onClick={clickHandler}>changeTitle</button>
-//       {/* <button onClick={() => {console.log("clicked ")}}> change title</button> */}
-//     </Card>
-//   );
-// };
